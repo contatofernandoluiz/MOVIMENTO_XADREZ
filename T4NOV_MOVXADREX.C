0f1a9c09d8801692823440c16ea143bf7d31153c
Fernando Rodrigues <contatofernandoluiz@gmail.com>
2025-09-16 11:37:20 -0300

    Add files via upload

--- T4NOV_MOVXADREX.C
+++ T4NOV_MOVXADREX.C
@@ -1,10 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    // SIMULAÇÃO DO MOVIMENTO DA  TORRE USANDO FOR
-    //TORRE MOVE 5 CASAS PARA DIREITA (HORIZONTAMENTE)
-    for (int i = 0; i < 5; i++){
-        printf("Direita\n");
+    // DEFININDO AS VARIAVEIS
+     int casasTorre = 5;
+     int casasBispo = 5;
+     int casasRainha =8;
+
+    int i; //VARIAVEL PARA LOOP
+
+    //MOVIMENTO DA TORRE EM FOR
+    printf("Movimento da torre (direita): \n");
+    for(i = 1; i <= casasTorre; i++){
+        printf("Direita \n");
     }
+    printf("\n");
+
+    //MOVIMENTO DO BISPO EM WHILE
+    printf("Movimento do Bispo (cima,direita): \n");
+    i = 1;
+    while (i <= casasBispo){
+        printf("Cima, direita\n");
+        i++;
+  
+    }
+    printf("\n");
+
+    //MOVIMENTO DA RAINHA EM DO-WHILE
+    printf("Movimento da Rainha (Esquerda): \n");
+    i = 1;
+
+    do
+    {
+        printf("Esquerda \n");
+        i++;
+    } while (i <= casasRainha);
+
+    printf("\n");
+    
+
     return 0;
 }
