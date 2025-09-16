809c89f6a9bf3c287204d87f25fe8a3d8696146e
Fernando Rodrigues <contatofernandoluiz@gmail.com>
2025-09-16 10:32:07 -0300

    Arquivo VS
    Arquivo do trabalho em C, movimento xadrez

---
+++ T4NOV_MOVXADREX.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // SIMULAÇÃO DO MOVIMENTO DA  TORRE USANDO FOR
+    //TORRE MOVE 5 CASAS PARA DIREITA (HORIZONTAMENTE)
+    for (int i = 0; i < 5; i++){
+        printf("Direita\n");
+    }
+    return 0;
+}
